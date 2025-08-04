df459cf196f3814adfdea104f66d6762fc4cd944
Harshvardhan <shreeharshpb11@gmail.com>
2025-08-05 00:07:53 +0530

    added router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/core/Navbar";
 
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
