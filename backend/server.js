17e6dc51ac7ebbfd9d288901a4d39fade0f47aa8
Harshvardhan <shreeharshpb11@gmail.com>
2025-08-06 00:37:42 +0530

    Setup backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+
+
+const PORT = 3333 || process.env.PORT;
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Server is working good',
+        user: 'Ohh! Nice one , I am also doing great'
+    })
+})
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
