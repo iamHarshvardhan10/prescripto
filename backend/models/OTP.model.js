62369b2d912653158da1e9b70b2445724f039de4
Harshvardhan <shreeharshpb11@gmail.com>
2025-08-12 00:08:41 +0530

    created sendVerification email and pre hook in otpShcema

--- backend/models/OTP.model.js
+++ backend/models/OTP.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import mailSender from "../utils/mailSender";
+import otpTemplate from "../mail/templates/emailVerificationTemplates";
 
 
 const otpSchema = new mongoose.Schema({
@@ -14,6 +16,27 @@ const otpSchema = new mongoose.Schema({
         type: Date,
         default: Date.now(),
         expires: 5 * 60
+    }
+})
+
+
+const sendVerificationEmail = async (email, otpViaEmail) => {
+    try {
+        const mailResponse = await mailSender(email, "OTP Verification Email", otpTemplate(otpViaEmail))
+        console.log(mailResponse.response)
+    } catch (error) {
+        console.log(error.message)
+        throw error;
+    }
+}
+
+
+
+// PRE HOOK
+otpSchema.pre("save", async function (next) {
+    console.log('New Document Saved To database')
+    if (this.isNew) {
+        await sendVerificationEmail(this.email, this.otpViaEmail)
     }
 })
 
