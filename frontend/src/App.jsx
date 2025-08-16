32dd12c331a5957555413510ac142195f76a63fd
Harshvardhan <shreeharshpb11@gmail.com>
2025-08-16 23:37:27 +0530

    reconfigure all setup with shadcn library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Navbar from "./components/core/Navbar";
-
+import Home from "./pages/Home";
 const App = () => {
   return (
     <div className="app">
---
+++ frontend/src/pages/Sign.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sign = () => {
+  return (
+    <div>fdjlk</div>
+  )
+}
+
+export default Sign
